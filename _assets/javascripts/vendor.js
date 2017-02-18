be6f5eab8ef9b74931d1c9d1fb37dc8e9a1e71ec
권용근 <kingbbode@gmail.com>
2017-02-18 18:41:04 +0900

    Site Change

---
+++ _assets/javascripts/vendor.js
@@ -0,0 +1,6 @@
+//= require jquery
+//= require vendor/jquery-throttle-debounce.js
+//= require fluidbox
+//= require scrollreveal
+//= require turbolinks5/dist/turbolinks.js
+//= require vendor/retina.js
